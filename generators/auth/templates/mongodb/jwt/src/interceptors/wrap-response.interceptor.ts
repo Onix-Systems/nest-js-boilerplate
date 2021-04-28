4706f2ee35f369d56271ff834af06536b747ea76
Yevhenii Filonenko <yevhenii.filonenko@onix-systems.com>
2021-04-28 17:15:26 +0300

    small refactor

--- generators/auth/templates/mongodb/jwt/src/interceptors/wrap-response.interceptor.ts
+++ generators/auth/templates/mongodb/jwt/src/interceptors/wrap-response.interceptor.ts
@@ -15,33 +15,35 @@ export default class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((...args) => {
-        const options: any = {};
-        if (args[0].data.length) {
-          if (args[0].data[0]._doc) {
-            options.attributes = Object.keys(_.omit(args[0].data[0]._doc, ['_id', 'id']));
+        const serializeOptions: any = {};
+        const { data, options, collectionName } = args[0];
+
+        if (data.length) {
+          if (data[0]._doc) {
+            serializeOptions.attributes = Object.keys(_.omit(data[0]._doc, ['_id', 'id']));
           } else {
-            options.attributes = Object.keys(_.omit(args[0].data[0], ['_id', 'id']));
+            serializeOptions.attributes = Object.keys(_.omit(data[0], ['_id', 'id']));
           }
 
-          args[0].data.forEach((item: any) => {
+          data.forEach((item: any) => {
             item.id = item._id;
             delete item._id;
           });
-        } else if (args[0].data._doc) {
-          options.attributes = Object.keys(_.omit(args[0].data._doc, ['_id', 'id']));
+        } else if (data._doc) {
+          serializeOptions.attributes = Object.keys(_.omit(data._doc, ['_id', 'id']));
         } else {
-          options.attributes = Object.keys(_.omit(args[0].data, ['_id', 'id']));
+          serializeOptions.attributes = Object.keys(_.omit(data, ['_id', 'id']));
         }
-        if (args[0].options) {
-          options.topLevelLinks = PaginationUtils.getPaginationLinks(
-            args[0].options.location,
-            args[0].options.paginationParams,
-            args[0].options.totalCount,
+        if (options) {
+          serializeOptions.topLevelLinks = PaginationUtils.getPaginationLinks(
+            options.location,
+            options.paginationParams,
+            options.totalCount,
           );
-          options.meta = { totalCount: args[0].options.totalCount };
+          serializeOptions.meta = { totalCount: options.totalCount };
         }
 
-        return new Serializer(args[0].collectionName, options).serialize(args[0].data);
+        return new Serializer(collectionName, serializeOptions).serialize(data);
       }),
     );
   }
